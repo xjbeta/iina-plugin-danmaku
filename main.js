4e761526b2323442127c10db9f2bf2042455d8dc
xjbeta <xjbeta.tiny@gmail.com>
2024-08-23 00:24:49 +0800

    fix: revert dash

--- main.js
+++ main.js
@@ -63,16 +63,17 @@ function removeOpts() {
 };
 
 function parseOpts() {
-    if (optsParsed) {
-        removeOpts();
+    if (optsParsed || mpv.getString('path') != "-") {
         return;
     };
 
     let scriptOpts = mpv.getString('script-opts').split(',');
-
     let iinaPlusValue = scriptOpts.find(s => s.startsWith(iinaPlusArgsKey));
-    if (iinaPlusValue && !stopped) {
+    
         optsParsed = true;
+    removeOpts();
+
+    if (iinaPlusValue && !stopped) {
 
         let opts = JSON.parse(hexToString(iinaPlusValue.substring(iinaPlusArgsKey.length)));
         print('iina plus opts: ' + JSON.stringify(opts));
@@ -262,7 +263,11 @@ iina.event.on("iina.window-did-close", () => {
 });
 
 function deinit() {
+    if (stopped) {
+        return;
+    };
     stopped = true;
+    setObserver(false);
     iinaPlusOpts = undefined;
     optsParsed = false;
     removeOpts();
@@ -281,8 +286,13 @@ iina.event.on("iina.file-started", () => {
     print('iina.file-started');
     stopped = false;
     let e = iina.preferences.get('enableIINAPLUSOptsParse');
-
-    parseOpts();
+    let p = mpv.getString('path');
+    if (p == "-") {
+        parseOpts();
+        return;
+    }
+    print('Ignore IINA+ Opts Parse, ' + p);
+    initMenuItems();
 });
 
 iina.event.on("mpv.pause.changed", (isPaused) => {
